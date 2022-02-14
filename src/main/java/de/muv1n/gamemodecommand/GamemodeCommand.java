baa27b2c2ab72026935da3520391dfcfb0623c4e
MuV1N <75033565+MuV1N@users.noreply.github.com>
2022-02-14 12:20:21 +0100

    Created Project.

---
+++ src/main/java/de/muv1n/gamemodecommand/GamemodeCommand.java
@@ -0,0 +1,17 @@
+package de.muv1n.gamemodecommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GamemodeCommand extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
